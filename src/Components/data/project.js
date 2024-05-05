fc016e3617252bce98bbb95820bef943a7e0614c
Dibakar <dibakarbera01@gmail.com>
2024-05-05 21:20:13 +0530

    update project

--- src/Components/data/project.js
+++ src/Components/data/project.js
@@ -1,5 +1,19 @@
 const project=[
     {
+        "name": "Freelance Website- PestStop Solution",
+        "description": "Pest Control Company Website",
+        "redirect_url": "https://peststopsolution.com/",
+        "link": "Live Link",
+        "image_url": "project/peststop.png"
+    },
+    {
+        "name": "Freelance Website - Cosmic Pest Solutions",
+        "description": "Pest Control Company Website",
+        "redirect_url": "https://cosmicpestsolutions.com",
+        "link": "Live Link",
+        "image_url": "project/cosmic.png"
+    },
+    {
         "name": "Blog Website",
         "description": "Micro Blogging web-application built with React, Storage/Authentication with Firebase.",
         "redirect_url": "https://dibakar-writenode-three.vercel.app",
@@ -14,18 +28,18 @@ const project=[
         "image_url": "project/project-movie.png"
     },
     {
-        "name": "Taskbox",
-        "description": "To do List Website where you can add, delete and edit the task using local storage and also change the theme.",
-        "redirect_url": "https://dibakar-taskbox-flax.vercel.app",
-        "link": "Live Link",
-        "image_url": "project/project-taskbox.png"
-    },
-    {
         "name": "Music Website UI",
         "description": "A basic introduction to nextjs with awesome UI from Aceternity UI",
         "redirect_url": "https://music-website-ui.pages.dev",
         "link": "Live Link",
         "image_url": "project/music_webiste_ui.png"
+    },
+    {
+        "name": "Taskbox",
+        "description": "To do List Website where you can add, delete and edit the task using local storage and also change the theme.",
+        "redirect_url": "https://dibakar-taskbox-flax.vercel.app",
+        "link": "Live Link",
+        "image_url": "project/project-taskbox.png"
     }
 ]
 export default project;
