701899668b56b7d69033dd8cbf7ae8b382ec7d41
Dibakar <dibakarbera01@gmail.com>
2024-05-09 00:52:27 +0530

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -18,15 +18,15 @@ const About = () => {
         </div> */}
         <div className="about-right">
             <div className="about-para">
-                <p>I am an experienced Frontend Developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
-                <p>My passion for frontend development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
+                <p>Full-stack developer specializing in accessible, visually appealing web applications. Certified in Core Java, MongoDB Node.js Developer Path and skilled in SDLC methodologies. Emphasizing strong communication, problem-solving, and teamwork for effective project delivery.</p>
+                <p>My passion for  development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
             </div>
             <div className="about-skills">
                 <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}} /></div>
-                <div className="about-skill"><p>Java</p><hr style={{width:"80%"}} /></div>
+                <div className="about-skill"><p>Java</p><hr style={{width:"70%"}} /></div>
                 <div className="about-skill"><p>React JS</p><hr style={{width:"70%"}} /></div>
                 <div className="about-skill"><p>JavaScript</p><hr style={{width:"60%"}} /></div>
-                <div className="about-skill"><p>Next JS</p><hr style={{width:"50%"}} /></div>
+                <div className="about-skill"><p>Next JS</p><hr style={{width:"65%"}} /></div>
             </div>
         </div>
       </div>
